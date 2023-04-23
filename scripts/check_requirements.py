62e8ad215d837431cd653a932ac5cc372b3be4dd
George Adams <george.adamopoulos13@gmail.com>
2023-04-23 10:45:38 -0400

    Cleaning up check_requirements.py
    Added docstrings, validated input, use more efficient set instead of dict, got rid of unecessary regex

--- scripts/check_requirements.py
+++ scripts/check_requirements.py
@@ -4,22 +4,22 @@ import sys
 import pkg_resources
 
 
-def main():
-    requirements_file = sys.argv[1]
+def check_requirements(requirements_file):
+    """
+    Check if the packages listed in a requirements file are installed.
+
+    :param requirements_file: path to the requirements file
+    :type requirements_file: str
+    """
     with open(requirements_file, "r") as f:
-        required_packages = [
-            line.strip().split("#")[0].strip() for line in f.readlines()
-        ]
+        requirements = list(pkg_resources.parse_requirements(f.read()))
 
-    installed_packages = [package.key for package in pkg_resources.working_set]
+    installed_packages = {package.key.lower() for package in pkg_resources.working_set}
 
-    missing_packages = []
-    for package in required_packages:
-        if not package:  # Skip empty lines
-            continue
-        package_name = re.split("[<>=@ ]+", package.strip())[0]
-        if package_name.lower() not in installed_packages:
-            missing_packages.append(package_name)
+    missing_packages = [
+        requirement.name for requirement in requirements
+        if requirement.name.lower() not in installed_packages
+    ]
 
     if missing_packages:
         print("Missing packages:")
@@ -29,5 +29,15 @@ def main():
         print("All packages are installed.")
 
 
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python check_requirements.py <requirements_file>")
+        sys.exit(1)
+
+    requirements_file = sys.argv[1]
+    check_requirements(requirements_file)
+
+
 if __name__ == "__main__":
     main()
+
